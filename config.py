dce4f5f27547f98633ad246666490fbb1bf3a64c
Tech2705 <160301845+Tech2705@users.noreply.github.com>
2024-10-08 16:21:54 +0530

    config.py

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ import os
 
 class Config(object):
     # env vars
-    BOT_TOKEN = "1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZ"  # string
-    API_ID = 1234567  # int
-    API_HASH = "123abc456def789ghi012jklmn"  # string
+    BOT_TOKEN = "7258465236:AAE-q_XyoQJczV6BcsNwTWn58gS_NeLCEBM"  # string
+    API_ID = 24792111  # int
+    API_HASH = "847de3c849cc0c68240aa5154a437bfe"  # string
     
     # db vars
     # keep empty if don't want to add any extra caption
     CAPTION_TEXT = "Added this caption"  
     # BOTTOM or TOP or NIL
     CAPTION_POSITION = "BOTTOM"  
-    ADMIN_USERNAME = "username"  # without "@". 
+    ADMIN_USERNAME = "triggeredsoul_07ded"  # without "@". 
 
     # a list of strings of words to remove from the existing caption
     WORDS_TO_REMOVE = []  
@@ -22,7 +22,7 @@ class Config(object):
 
     # keep empty to allow in all channels. Can add multiple channels separated by a comma.
     # Don't forget -100 before the channel ID
-    ALLOWED_CHANNELS = [-100123456789]
+    ALLOWED_CHANNELS = [-1002166283591]
 
     # REMOVE or POSTFIX or NIL. Useful for tamilblasters, tamilmv and other webites
     WEBSITE_PREFIX = "POSTFIX"  
