88c78aa30dd29df654b14fc431b91efd4627aafd
Tech2705 <160301845+Tech2705@users.noreply.github.com>
2025-08-11 16:24:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ import os
 
 class Config(object):
     # env vars
-    BOT_TOKEN = "7258465236:AAE-q_XyoQJczV6BcsNwTWn58gS_NeLCEBM"  # string
-    API_ID = 24792111  # int
-    API_HASH = "847de3c849cc0c68240aa5154a437bfe"  # string
+    BOT_TOKEN = ""  # string
+    API_ID =   # int
+    API_HASH = ""  # string
     
     # db vars
     # keep empty if don't want to add any extra caption
     CAPTION_TEXT = "Added this caption"  
     # BOTTOM or TOP or NIL
     CAPTION_POSITION = "BOTTOM"  
-    ADMIN_USERNAME = "triggeredsoul_07ded"  # without "@". 
+    ADMIN_USERNAME = ""  # without "@". 
 
     # a list of strings of words to remove from the existing caption
     WORDS_TO_REMOVE = []  
